f417abee6248bcf2920f58f60169f0e0f229c4c6
Dean Brettle <dean@brettle.com>
2015-04-25 22:46:23 -0700

    Convert makeyourbed.js to Typescript.

--- makeyourbed.ts
+++ makeyourbed.ts
@@ -1,14 +1,17 @@
+/// <reference path="typings/meteor/meteor.d.ts"/>
+
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
+  var helloTemplate: Template = Template["hello"];
+  helloTemplate.helpers({
     counter: function () {
       return Session.get('counter');
     }
   });
 
-  Template.hello.events({
+  helloTemplate.events({
     'click button': function () {
       // increment the counter when button is clicked
       Session.set('counter', Session.get('counter') + 1);
