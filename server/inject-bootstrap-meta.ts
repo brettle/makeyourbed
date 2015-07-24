5861121b021a525cef29000d5743ce9c31242474
Dean Brettle <dean@brettle.com>
2015-07-24 16:54:25 -0700

    Use brettle:bootstrap-with-injected-meta-tags

--- server/inject-bootstrap-meta.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="../typings/inject-initial/inject-initial.d.ts"/>
-Inject.rawHead('bootstrap-meta', `  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-  <!-- Bootstrap -->
-  `);
--- typings/inject-initial/inject-initial.d.ts
+++
@@ -1,4 +0,0 @@
-declare module Inject {
-  function meta(id:string, textOrFunc:string|Function, response?): void;
-  function rawHead(id:string, textOrFunc:string|Function, response?): void;
-}
